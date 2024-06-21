15f58dd6d85673cbf3110dc5b06ff03c8e8039ac
HARINI S <167424534+Harini-S109@users.noreply.github.com>
2024-06-21 12:41:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,36 @@ const totalP = document.querySelector(".tot-p")
 const btn = document.querySelectorAll("#btn")
 const custom = document.querySelector("#custom")
 const reset = document.querySelector("#reset-btn")
+const error  = document.getElementById("error")
 let Tip = 0
 let Total = 0
 
 
+if( persons.value === 0){
+    error.style.display = "block"
+}
+
 persons.addEventListener('input', (e) => {
 
+    if( parseInt(persons.value) === 0 ){
+        error.style.display = "block"
+    }
+    else{
+        
     btn.forEach((btn) =>{
+        
         btn.addEventListener('click',(e) => {
-           const btnValue =  e.currentTarget.value
+            if( parseInt(persons.value) === 0 || persons.value === " "){
+                error.style.display = "block"
+            }
+            else{
             Tip = parseFloat(((e.currentTarget.value)/100) * parseFloat(bill.value) / parseInt(persons.value) )
-            totalTip.innerText = `$${Tip}`
+            totalTip.innerText = `$${Tip.toFixed(2)}`
             let Tot = Total + Tip
            totalP.innerText = `$${Tot.toFixed(2)} `
            Tip = 0
-        })
+            }
+        }) 
     })
 
     Total = parseFloat(parseFloat(bill.value) / parseInt(persons.value) ) + Tip
@@ -29,17 +44,19 @@ persons.addEventListener('input', (e) => {
 
     custom.addEventListener('input',(e) => {
             Tip = parseFloat(((e.currentTarget.value)/100) * parseFloat(bill.value) / parseInt(persons.value) )
-            totalTip.innerText = `$${Tip}`
+            totalTip.innerText = `$${Tip.toFixed(2)}`
             let Tot = Total + Tip
            totalP.innerText = `$${Tot.toFixed(2)} `
            Tip = 0
         })
+    }
 })
 
 reset.addEventListener('click',(e) =>{
     bill.value = "0"
     persons.value = "0"
-    custom.value = "0"
+    error.style.display = "none"
+    custom.placeholder = "0"
     totalTip.innerText = `$0`
     totalP.innerText = `$0`
 })
